d49dedb0e1eaa0c3d09dc74e5c3db6ba1cc14577
Tanya Singh <tanyacs27@gmail.com>
2020-03-16 00:02:08 +0530

    Bubble Sort Algorithm

---
+++ Sorting/Bubble Sort.js
@@ -0,0 +1,16 @@
+function BubbleSort(arr){
+    let n = arr.length-1;
+    while(n>=0){
+        for(let i=0; i<=n ; i++)
+         {
+            if(arr[i] > arr[i+1])
+            {
+              [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+            }           
+          }                       
+        n--;
+      }
+      return arr;
+  }
+  
+  console.log(BubbleSort([7,8,1,5,3,0,9,4]));
