909b025ba2b8d0dc792fef009a7190f78daa6e22
Tanya Singh <tanyacs27@gmail.com>
2020-03-19 23:51:06 +0530

    Insertion Sort

---
+++ Sorting/Insertion Sort.js
@@ -0,0 +1,13 @@
+function InsertSort(arr){
+    for(let i = 1 ; i < arr.length ; i++){
+        let Current = arr[i];
+        for(var j = i-1 ; j >= 0 && arr[j] > Current ; j--){
+            arr[j+1] = arr[j];
+            console.log(arr);
+        }
+        arr[j+1] = Current;
+    }
+    return arr;
+}
+
+console.log(InsertSort([0,2,35,22,10,18,16]));
