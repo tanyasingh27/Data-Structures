eb8e847f85217ba6eddf2c844c0b2928bfdfe22c
Tanya Singh <tanyacs27@gmail.com>
2020-04-20 20:17:16 +0530

    Binary Search Tree Implementation

---
+++ Tree/BinarySearchTree.js
@@ -0,0 +1,50 @@
+/******************** Binary Search Tree **********************/
+class Node{   
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+
+    insert(val){
+        let newNode = new Node(val);
+        let current = this.root;
+        while(current){
+            if(val < current.value){
+                if(!current.left) {
+                    current.left = newNode;
+                    return this;
+                }
+                current= current.left;
+            }
+            else if(val > current.value){
+                if(!current.right) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+            else{
+                return undefined;
+            }
+        }
+        this.root = newNode;
+        return this;
+               
+    }
+}
+
+/************** Test *****************/
+let bst = new BinarySearchTree();
+bst.insert(41);
+bst.insert(62)
+bst.insert(32);
+bst.insert(22);
+bst.insert(38);
+bst.insert(37);
