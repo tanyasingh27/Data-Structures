a14d72b261553231594197abb3296ec80582cb0c
Tanya Singh <tanyacs27@gmail.com>
2020-06-27 22:12:31 +0530

    Max Binary Extract Max

--- Binary Heaps/MaxBinaryHeap.js
+++ Binary Heaps/MaxBinaryHeap.js
@@ -18,6 +18,46 @@ class MaxBinaryHeap {
             index = parentIndex;            
         }
     }
+
+    extractMax(){
+        const max = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+    sinkDown(){
+        let index = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true){
+            let leftChildIndex = (2 * index) + 1;
+            let rightChildIndex = (2 * index) + 2;
+            let leftChild, rightChild, swap = null;
+            if(leftChildIndex < length){
+                leftChild = this.values[leftChildIndex];
+                if(leftChild > element){
+                    swap = leftChildIndex;
+                }                
+            }
+            if(rightChildIndex < length){
+                rightChild = this.values[rightChildIndex];
+                if(
+                    (swap === null && rightChild > element)|| 
+                    (swap !== null && rightChild > leftChild)
+                ){
+                    swap = rightChildIndex;
+                }                
+            }
+
+            if(swap === null) break;
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+    }
 }
 
 let maxHeap = new MaxBinaryHeap();
@@ -29,4 +69,12 @@ maxHeap.insert(27);
 maxHeap.insert(12);
 console.log(maxHeap);
 maxHeap.insert(55);
+console.log(maxHeap);
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
+maxHeap.extractMax();
 console.log(maxHeap);
